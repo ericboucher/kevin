8a9430996e87ebccfb0bf493f63b9c2d8decc473
ericboucher <eric.boucher@spire.com>
2016-04-27 20:19:50 -0700

    Comment out connection test and change output file name

--- kevin-twitter-access.py
+++ kevin-twitter-access.py
@@ -24,19 +24,21 @@ api = tweepy.API(auth)
 def process_or_store(tweet):
     print(json.dumps(tweet))
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    process_or_store(status._json)
+
+#for status in tweepy.Cursor(api.home_timeline).items(1):
+#    # Process a single status
+#    process_or_store(status._json)
 
 class MyListener(StreamListener):
  
     def on_data(self, data):
         try:
-            with open('python.json', 'a') as f:
+            with open('python_stream.json', 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
+            time.sleep(5)
         return True
  
     def on_error(self, status):
