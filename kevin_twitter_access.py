1d59a8ecd8c19914558d7cc745dd8c3c348f3651
ericboucher <eric.boucher@spire.com>
2016-05-06 18:52:04 -0700

    async stream process

--- kevin_twitter_access.py
+++ kevin_twitter_access.py
@@ -1,112 +1,149 @@
-# Kevin the Scrapper
+""" This module defines the base functions to interact with the twitter API.""" # Kevin the Scrapper
 import time
-import tweepy
 import json
-import utils
-import datetime
+import tweepy
+#import utils
+#import datetime
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+#from Filters import Filter, FiltersList
 
 # Authentication
-with open("./client_secrets_twitter.json") as data_file:    
-    client_secrets_twitter = json.load(data_file)
+with open("./client_secrets_twitter.json") as data_file:
+    CLIENT_SECRETS_TWITTER = json.load(data_file)
 
-consumer_key = client_secrets_twitter['consumer_key']
-consumer_secret = client_secrets_twitter['consumer_secret']
-access_token = client_secrets_twitter['access_token']
-access_secret = client_secrets_twitter['access_secret']
- 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth)
+CONSUMER_KEY = CLIENT_SECRETS_TWITTER['consumer_key']
+CONSUMER_SECRET = CLIENT_SECRETS_TWITTER['consumer_secret']
+ACCESS_TOKEN = CLIENT_SECRETS_TWITTER['access_token']
+ACCESS_SECRET = CLIENT_SECRETS_TWITTER['access_secret']
 
-## Test Auth
+AUTH = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+AUTH.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
 def process_or_store(tweet):
-    print(json.dumps(tweet))
+    """Process tweet"""
+    print json.dumps(tweet)
+
+def process_tweet(data):
+    """process raw tweet"""
+    mtweet = json.loads(data)
+    # We only want to write tweets; we'll skip 'delete' and
+    # 'limit' information.
+    if 'delete' in mtweet:
+        print "contained delete"
+        return True
+    if 'limit' in mtweet:
+        return True
+
+    with open('twitter_stream_trending.json', 'a') as destination_file:
+        dump = {}
+        dump['created_at'] = mtweet['created_at']
+        ## TO DO - Change as Timestamp
+        ## TO DO - Add trend #hashtag
+        dump['text'] = mtweet['text'].encode('ascii', 'ignore')
+        #print dump
+        destination_file.write(json.dumps(dump))
+        destination_file.write("\n")
 
 def initialize_api():
-    return tweepy.API(auth), auth
+    """Initialize an API
+    :return: tweepy api and auth
+    """
+    return tweepy.API(AUTH), AUTH
 
-#for status in tweepy.Cursor(api.home_timeline).items(1):
-#    # Process a single status
-#    process_or_store(status._json)
+def get_timeline_status():
+    return "hello"
+    #for status in tweepy.Cursor(api.home_timeline).items(1):
+    #    # Process a single status
+    #    process_or_store(status._json)
 
-count = 0
+class SimpleListener(StreamListener):
+    """StreamListener implementation"""
+    def __init__(self):
+        StreamListener.__init__(self)
+        self.count = 0
 
-class MyListener(StreamListener):
     def on_data(self, data):
-        global count
-        mtweet = json.loads(data)
-        # We only want to write tweets; we'll skip 'delete' and
-        # 'limit' information.
-        if 'delete' in mtweet:
-            print "contained delete"
-            return True
-        if 'limit' in mtweet:
-            return True
-
-
-        try:
-            with open('twitter_stream_trending.json', 'a') as f:
-                dump = {}
-                dump['created_at'] = mtweet['created_at']
-                ## TO DO - Change as Timestamp
-                ## TO DO - Add trend #hashtag
-                dump['text'] = mtweet['text'].encode('ascii', 'ignore')
-                f.write(json.dumps(dump))
-                f.write("\n")
-                count+=1
-                if count%10 == 0:
-                    print "%d Tweets Saved"%count
-                return True
-        except BaseException as e:
-            print("Error on_data: %s" % str(e))
-            time.sleep(5)
+        #try:
+        process_tweet(data)
+        self.count += 1
+        if self.count%10 == 0:
+            print "%d Tweets Processed"%self.count
         return True
- 
-    def on_error(self, status):
-        print(status)
-        return True
+        #except BaseException as base_error:
+        #    print "Error on_data: %s" % str(base_error)
+        #    time.sleep(5)
+        #   return True
 
-class RegisterTweet(StreamListener):
-
-    #TODO - Change this function to register new requests from users, or remove
-    # Option1 - Hey @KevinBot, please follow #XXX.
-    # Option2 - Thanks @kevinBot, I'm all set.
- 
-    def on_data(self, data):
- 
-    jsonData = json.loads(data.strip())
-     
-    retweeted = tweet.get('retweeted', False)
-    from_self = tweet.get('user',{}).get('id_str','') == account_user_id
- 
-    if retweeted is not None and not retweeted and not from_self:
- 
-        tweetId = jsonData.get('id_str')
-        screenName = jsonData.get('user').get('screen_name')
-        tweetText = jsonData.get('text')
- 
-        chatResponse = chatbot.respond(tweetText)
-        # process stream data here
-
-        replyText = '@' + screenName + ' ' + chatResponse
-     
-    if len(replyText) > 140:
-            replyText = replyText[0:137] + '...'
- 
-    twitterApi.update_status(replyText, tweetId)
- 
     def on_error(self, status):
         print status
- 
+        return True
 
-hashtags_list = [u'#WHCD', u'#5WordLieToYourSpouse', u'#RDMA', u'#VindictiveSongs', u'#IfIMadeAMovie', u'#MikeysNewVideo', u'#independentbookstoreday', u'#stumpthetruck', u'#SparksEnergy300', u'#iHeartCountry']
+# class RegisterTweet(StreamListener):
+#     """Another Listener"""
+#     # todo - Change this function to register new requests from users, or remove
+#     # Option1 - Hey @KevinBot, please follow XXX.
+#     # Option2 - Thanks @kevinBot, I'm all set.
 
-def create_stream(hashtags_list = []):
-    twitter_stream = Stream(auth, MyListener())
-    twitter_stream.filter(track=hashtags_list)
+#     def on_data(self, data):
 
-### Link for streaming process: https://gist.github.com/bonzanini/af0463b927433c73784d
+#         json_data = json.loads(data.strip())
+
+#         retweeted = json_data.get('retweeted', False)
+#         from_self = json_data.get('user', {}).get('id_str', '') == "KEVIN_TWITTER_ID"
+
+#         if retweeted is not None and not retweeted and not from_self:
+
+#             tweet_id = json_data.get('id_str')
+#             screen_name = json_data.get('user').get('screen_name')
+#             tweet_text = json_data.get('text')
+
+#             print tweet_id, screen_name, tweet_text
+#             #chatResponse = chatbot.respond(tweetText)
+#             # process stream data here
+
+#             #replyText = '@' + screen_name + ' ' + chatResponse
+
+#         #if len(replyText) > 140:
+#         #        replyText = replyText[0:137] + '...'
+#         #API.update_status(replyText, tweet_id)
+
+#     def on_error(self, status):
+#         print status
+
+HASHTAGS_LIST = [u'#WHCD', u'#5WordLieToYourSpouse', u'#RDMA', u'#VindictiveSongs',
+                 u'#IfIMadeAMovie', u'#MikeysNewVideo', u'#independentbookstoreday',
+                 u'#stumpthetruck', u'#SparksEnergy300', u'#iHeartCountry']
+
+def get_trends():
+    """ This functions allows you to get trends"""
+    api, _ = initialize_api()
+
+    trends_us = api.trends_place(id=23424977)
+    # TO DO - Order By tweet_volume
+    hashtags = [x['name'] for x in trends_us[0]['trends'] if x['name'].startswith('#')]
+    trends_hashtag = hashtags[0:10]
+    return trends_hashtag
+
+def create_stream(hashtags_list=["#python"]):
+    """stream_creator"""
+    twitter_stream = Stream(AUTH, SimpleListener())
+    twitter_stream.filter(track=hashtags_list, async=True)
+    return twitter_stream
+
+def self_updating_trendy_stream():  
+    trends_list = get_trends()
+    twitter_stream = create_stream(trends_list)
+    while True:
+        new_trends = get_trends()
+        if trends_list != new_trends:
+            print "Updating List of trends"
+            trends_list = new_trends
+            twitter_stream.disconnect()
+            twitter_stream.filter(track=trends_list, async=True)
+        time.sleep(60)
+
+if __name__ == "__main__":
+    #filters_list = FiltersList([Filter("#python", 1)])
+    self_updating_trendy_stream()
--- kevin_twitter_get_trends.py
+++ kevin_twitter_get_trends.py
@@ -1,15 +1,16 @@
-import time
-import tweepy
-import json
-from kevin_twitter_access import initialize_api, create_stream
- 
-api, auth = initialize_api()
+""" This module allows to fetch the last trends trends"""
+from kevin_twitter_access import initialize_api
 
-trendsUS = api.trends_place(id = 23424977)
-# TO DO - Order By tweet_volume
-hashtags = [x['name'] for x in trendsUS[0]['trends'] if x['name'].startswith('#')]
-print hashtags
-trends_hashtag = hashtags[0:10]
+def get_trends():
+    """ This functions allows you to get trends"""
+    api, _ = initialize_api()
 
-# Create Stream
-create_stream(trends_hashtag)
+    trends_us = api.trends_place(id=23424977)
+    # TO DO - Order By tweet_volume
+    hashtags = [x['name'] for x in trends_us[0]['trends'] if x['name'].startswith('#')]
+    print hashtags
+    trends_hashtag = hashtags[0:10]
+    return trends_hashtag
+
+if __name__ == "__main__":
+    get_trends()
